dac9c0d2b4bfefc8ae610d1c4a4202455efbb51a
Bill Cheng <so89898@gmail.com>
2025-08-08 00:11:40 +0800

    Initial Commit

---
+++ XcodePAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodePAI
+//
+//  Created by Bill Cheng on 2025/8/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodePAI/XcodePAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodePAIApp.swift
+//  XcodePAI
+//
+//  Created by Bill Cheng on 2025/8/8.
+//
+
+import SwiftUI
+
+@main
+struct XcodePAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
