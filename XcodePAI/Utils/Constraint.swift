a0fbe12fb65af5facb37614dacebf6fc93fe3af9
Bill Cheng <so89898@gmail.com>
2025-08-11 16:57:52 +0800

    Add LF const

--- XcodePAI/Utils/Constraint.swift
+++ XcodePAI/Utils/Constraint.swift
@@ -10,12 +10,14 @@ import Foundation
 class Constraint {
     // Static strings
     static let AppName = "XcodePaI"
+    static let LFString = "\n"
     static let CRLFString = "\r\n"
     static let DoubleCRLFString = "\r\n\r\n"
     
     static let DoubleLFString = "\n\n"
     
     //Static Data
+    static let LF = Data(Constraint.LFString.utf8)
     static let CRLF = Data(Constraint.CRLFString.utf8)
     static let DoubleCRLF = Data(Constraint.DoubleCRLFString.utf8)
     static let DoubleLF = Data(Constraint.DoubleLFString.utf8)
