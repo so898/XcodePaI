2f45d2c1bd6c8645d70de67aacd5d9147ee124ba
Bill Cheng <so89898@gmail.com>
2025-08-13 02:05:43 +0800

    Add string hash function

--- XcodePAI/Utils/Extensions.swift
+++ XcodePAI/Utils/Extensions.swift
@@ -6,9 +6,22 @@
 //
 
 import Foundation
+import CryptoKit
+import CommonCrypto
 
 extension Date {
     static func currentTimeStamp() -> Int{
         return Int(Date().timeIntervalSince1970)
+    }
+}
+
+extension String {
+    func sha256() -> String {
+        guard let data = self.data(using: .utf8) else { return self }
+        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
+        }
+        return hash.map { String(format: "%02x", $0) }.joined()
     }
 }
