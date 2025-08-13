5e3fe629e9171541ea3f5232422334fee708c352
Bill Cheng <so89898@gmail.com>
2025-08-13 19:54:08 +0800

    Update model provider view.

--- XcodePAI/Views/SettingsView.swift
+++ XcodePAI/Views/SettingsView.swift
@@ -21,7 +21,7 @@ struct SettingsView: View {
     // Tab Infos
     let tabs = [
         TabItem(id: 0, title: "General", imageName: "gearshape"),
-        TabItem(id: 1, title: "LLM", imageName: "wand.and.stars"),
+        TabItem(id: 1, title: "Provider", imageName: "sparkles.square.filled.on.square"),
         TabItem(id: 2, title: "Chat Proxy", imageName: "chart.bar.horizontal.page"),
         TabItem(id: 3, title: "Completion", imageName: "pencil.and.list.clipboard"),
         TabItem(id: 4, title: "About", imageName: "info.circle.fill"),
@@ -33,7 +33,7 @@ struct SettingsView: View {
                 Tab(tab.title, systemImage: tab.imageName) {
                     switch tab.id {
                     case 0: GeneralSettingSectionView()
-                    case 1: LLMSettingSectionView()
+                    case 1: ModelProviderSettingSectionView()
                     case 2: ChatProxySettingSectionView()
                     default: GeneralSettingSectionView()
                     }
