18fc56cfa0c6f4e939924f8c893e047ba4e585d8
Bill Cheng <so89898@gmail.com>
2025-08-15 18:19:40 +0800

    Add MCP models.

---
+++ XcodePAI/Models/LLMMCP.swift
@@ -0,0 +1,76 @@
+//
+//  LLMMCP.swift
+//  XcodePAI
+//
+//  Created by Bill Cheng on 8/15/25.
+//
+
+import Foundation
+
+class LLMMCP: Identifiable, ObservableObject, Codable {
+    var id = UUID()
+    let name: String
+    let url: String
+    let description: String?
+    let headers: [String: String]?
+    @Published var enabled: Bool
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case name
+        case url
+        case description
+        case headers
+        case enabled
+    }
+    
+    init(id: UUID = UUID(), name: String, url: String, description: String? = nil, headers: [String: String]? = nil, enabled: Bool = true) {
+        self.id = id
+        self.name = name
+        self.url = url
+        self.description = description
+        self.headers = headers
+        self.enabled = enabled
+    }
+    
+    // MARK: - Codable
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        url = try container.decode(String.self, forKey: .url)
+        description = try container.decodeIfPresent(String.self, forKey: .description)
+        headers = try container.decodeIfPresent([String: String].self, forKey: .headers)
+        enabled = try container.decode(Bool.self, forKey: .enabled)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(url, forKey: .url)
+        try container.encodeIfPresent(description, forKey: .description)
+        try container.encodeIfPresent(headers, forKey: .headers)
+        try container.encode(enabled, forKey: .enabled)
+    }
+    
+    // MARK: - Utilities
+    public func toDictionary() -> [String: Any] {
+        var ret: [String: Any] = [
+            "id": id,
+            "name": name,
+            "url": url,
+            "enabled": enabled
+        ]
+        
+        if let description = description {
+            ret["description"] = description
+        }
+        
+        if let headers = headers {
+            ret["headers"] = headers
+        }
+        
+        return ret
+    }
+}
---
+++ XcodePAI/Models/LLMMCPTool.swift
@@ -0,0 +1,71 @@
+//
+//  LLMMCPTool.swift
+//  XcodePAI
+//
+//  Created by Bill Cheng on 8/15/25.
+//
+
+import Foundation
+import MCP
+
+class LLMMCPTool: Identifiable, Codable {
+    var id = UUID()
+    
+    let name: String
+    let description: String
+    let schema: String?
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case name
+        case description
+        case schema
+    }
+    
+    init(name: String, description: String, schema: String? = nil) {
+        self.name = name
+        self.description = description
+        self.schema = schema
+    }
+    
+    init(tool: Tool) {
+        self.name = tool.name
+        self.description = tool.description
+        if let jsonData = try? JSONEncoder().encode(tool.inputSchema), let schema = String(data: jsonData, encoding: .utf8) {
+            self.schema = schema
+        } else {
+            self.schema = nil
+        }
+    }
+    
+    // MARK: - Codable
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        description = try container.decode(String.self, forKey: .description)
+        schema = try container.decodeIfPresent(String.self, forKey: .schema)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(description, forKey: .description)
+        try container.encodeIfPresent(schema, forKey: .schema)
+    }
+    
+    func toPrompt() -> String {
+        var ret = """
+            <tool>
+            <name>\(name)</name>
+            <description>\(description)</description>
+            """
+        if let schema = schema {
+            ret += "\n<arguments>\(schema)</arguments>"
+        }
+        ret += "\n</tool>"
+        
+        return ret
+    }
+}
